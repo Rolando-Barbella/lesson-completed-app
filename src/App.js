5e1b215ffc829457ed730d3a7b19e7a2731a3c97
Rolando-Barbella <rolandobarbella@gmail.com>
2019-12-31 19:16:06 -0400

    lesson-7-half

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Buttons';
 
 function App() {
+  const [title] = useState('Mi aplicacion React');
+  const [showTitle, setShowTile] = useState(true); 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h3 className="Title">
+          {showTitle &&
+            title
+          }
+        </h3>
+        <Button
+         type="primary"
+         onClick={() => setShowTile(!showTitle)}
         >
-          Learn React
-        </a>
+          {showTitle? 'Ocultar' : 'Mostrar'}
+        </Button>
       </header>
     </div>
   );
---
+++ src/components/Buttons.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ children, onClick, type }) => {
+  return(
+    <button
+      className={`App-btn ${type === 'primary'? 'primary': 'default'}`}
+      onClick={() => onClick()}
+    > 
+      {children}
+    </button>
+  )
+};
+
+Button.propTypes = {
+  children: PropTypes.any.isRequired,
+  onClick: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(["primary", "default"]),
+};
+
+Button.defaultProps = {
+  type: 'default',
+}
+
+export default Button;
