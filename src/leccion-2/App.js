b8be7dfd35b15a4882446037a3fdc01ac3800dd8
Rolando-Barbella <rolandobarbella@gmail.com>
2020-04-14 15:20:14 +0530

    Added lesson 2 folder

---
+++ src/leccion-2/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  const lessonsList = [
+    { id: 1, lesson: 'State', completed: false },
+    { id: 2, lesson: 'UseRef', completed: false },
+    { id: 3, lesson: 'Hooks personalizados', completed: false },
+    { id: 4, lesson: 'Eventos', completed: false },
+    { id: 5, lesson: 'Pulir aplicación', completed: false },
+  ];
+
+  const [lessons, setCompleted] = useState(lessonsList);
+
+  const onCompleted = (id, completed) => {
+    const updatedList = lessons.map(lesson => {
+      if(lesson.id === id) {
+        lesson.completed = !completed;
+      }
+      return lesson
+    });
+    setCompleted(updatedList);
+  }
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3 className="Title">
+          Mi aplicacion React
+        </h3>
+          {
+            lessons.map(({ lesson, id, completed }) => (
+              <label key={id}>
+                <input 
+                  type="checkbox"
+                  onChange={() => onCompleted(id, completed)}
+                />
+                <span/>
+                {lesson}
+              </label>
+            ))
+          }
+      </div>
+    </div>
+  );
+}
+
+export default App;
