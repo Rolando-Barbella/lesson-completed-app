465d288f208aa7b01b590399d864b9ed8d920297
Rolando-Barbella <rolandobarbella@gmail.com>
2020-04-15 14:29:54 +0530

    Added lesson  folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './leccion-4/App';
+import App from './leccion-5/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
